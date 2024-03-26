5ecee19163adf49c2dca1e72dc9a1ad249d3f450
Lahesh <laheshk@gmail.com>
2024-03-26 20:46:07 +0000

    Initial Commit

---
+++ iOSGenie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSGenie
+//
+//  Created by Lahesh on 26/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSGenie/iOSGenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSGenieApp.swift
+//  iOSGenie
+//
+//  Created by Lahesh on 26/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSGenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
